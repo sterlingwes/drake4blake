857c060c25614c38e033f7a46f6b23826ed6a32c
Derek Hendrickx <derek.hendrickx@gmail.com>
2016-10-22 21:07:56 -0400

    Improve Game Manager

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Map = require('./Map')
 const MAX_NB_PERSONS = 10
 
 let drake
+let persons
 
 var game = window.game = new Phaser.Game(960, 800, Phaser.AUTO, 'app', {
   preload: preload,
@@ -13,22 +14,31 @@ var game = window.game = new Phaser.Game(960, 800, Phaser.AUTO, 'app', {
 })
 
 function preload () {
-  this.map = Map.init()
-  this.drake = new Drake()
-  this.persons = []
-  for (let i = 0; i < MAX_NB_PERSONS; i++) {
-    this.persons.push(new Person())
-  }
+  Map.init()
+  drake = new Drake()
+  persons = []
 }
 
 function create () {
-  this.map.create()
-  this.drake.create()
-  this.persons.forEach(person => { person.create() })
+  Map.create()
+  drake.create()
+  initPersons()
 }
 
 function update () {
-  this.map.update()
-  this.drake.update()
-  this.persons.forEach(person => { person.update() })
+  Map.update()
+  drake.update()
+  persons.forEach(person => { person.update() })
+}
+
+function initPersons() {
+  const tiles = Map.tiles
+  for (let i = 0; i < MAX_NB_PERSONS; i++) {
+    const personTemp = new Person()
+    personTemp.create()
+    while (persons.findIndex(person => person.comparePosition(personTemp)) > -1) {
+      personTemp.setPosition()
+    }
+    persons.push(personTemp)
+  }
 }
